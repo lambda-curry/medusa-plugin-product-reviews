fded25b65645e3cf5fc399e5c943ed058229455d
togoog1 <nathanallen888@gmail.com>
2023-08-10 11:44:33 -0500

    fix (product-review-request) add in constructor imports

--- packages/plugin/src/services/product-review-request.ts
+++ packages/plugin/src/services/product-review-request.ts
@@ -5,12 +5,12 @@ import {
   Selector,
   TransactionBaseService,
   buildQuery,
-} from '@medusajs/medusa';
-import { EntityManager } from 'typeorm';
-import FileService from '@medusajs/medusa/dist/services/file';
-import { ProductReviewRequestRepository } from '../repositories';
-import { ProductReviewRequest } from '../models';
-import { MedusaError, isDefined } from 'medusa-core-utils';
+} from "@medusajs/medusa";
+import { EntityManager } from "typeorm";
+import FileService from "@medusajs/medusa/dist/services/file";
+import { ProductReviewRequestRepository } from "../repositories";
+import { ProductReviewRequest } from "../models";
+import { MedusaError, isDefined } from "medusa-core-utils";
 
 type InjectedDependencies = {
   manager: EntityManager;
@@ -21,7 +21,7 @@ type InjectedDependencies = {
 };
 
 class ProductReviewRequestService extends TransactionBaseService {
-  static readonly resolutionKey = 'productReviewRequestService';
+  static readonly resolutionKey = "productReviewRequestService";
 
   protected manager: EntityManager;
   protected readonly orderService: OrderService;
@@ -30,55 +30,41 @@ class ProductReviewRequestService extends TransactionBaseService {
   private readonly productReviewRequestRepository_: typeof ProductReviewRequestRepository;
 
   static readonly Events = {
-    CREATED: 'product_review_request.created',
-    DELETED: 'product_review_request.deleted',
+    CREATED: "product_review_request.created",
+    DELETED: "product_review_request.deleted",
   };
 
   constructor(container: InjectedDependencies) {
     super(arguments[0]);
     this.manager_ = container.manager;
-    this.productReviewRequestRepository_ =
-      container.productReviewRequestRepository;
+    this.productReviewRequestRepository_ = container.productReviewRequestRepository;
     this.orderService = container.orderService;
+    this.eventBusService = container.eventBusService;
   }
 
   async create(order_id: string) {
-    return await this.atomicPhase_(async manager_ => {
-      const repo = manager_.withRepository(
-        this.productReviewRequestRepository_
-      );
+    return await this.atomicPhase_(async (manager_) => {
+      const repo = manager_.withRepository(this.productReviewRequestRepository_);
 
       const order = await this.orderService.retrieve(order_id);
 
-      if (!order)
-        throw new MedusaError(MedusaError.Types.NOT_FOUND, 'Order not found');
+      if (!order) throw new MedusaError(MedusaError.Types.NOT_FOUND, "Order not found");
 
-      const request = await repo.save({ order_id });
+      const request = await repo.save(repo.create({ order_id }));
 
-      await this.eventBusService
-        .withTransaction(manager_)
-        .emit(ProductReviewRequestService.Events.CREATED, {
-          id: request.id,
-          order_id: request.order_id,
-        });
+      await this.eventBusService.withTransaction(manager_).emit(ProductReviewRequestService.Events.CREATED, {
+        id: request.id,
+        order_id: request.order_id,
+      });
 
       return request;
     });
   }
 
-  async retrieve(
-    requestId: string,
-    config: FindConfig<ProductReviewRequest> = {}
-  ): Promise<ProductReviewRequest> {
-    const repo = this.activeManager_.withRepository(
-      this.productReviewRequestRepository_
-    );
+  async retrieve(requestId: string, config: FindConfig<ProductReviewRequest> = {}): Promise<ProductReviewRequest> {
+    const repo = this.activeManager_.withRepository(this.productReviewRequestRepository_);
 
-    if (!isDefined(requestId))
-      throw new MedusaError(
-        MedusaError.Types.NOT_FOUND,
-        `"requestId" must be defined`
-      );
+    if (!isDefined(requestId)) throw new MedusaError(MedusaError.Types.NOT_FOUND, `"requestId" must be defined`);
 
     const query = buildQuery({ id: requestId }, config);
 
@@ -91,9 +77,7 @@ class ProductReviewRequestService extends TransactionBaseService {
     selector: Selector<ProductReviewRequest> = {},
     config?: FindConfig<ProductReviewRequest>
   ): Promise<ProductReviewRequest[]> {
-    const repo = this.activeManager_.withRepository(
-      this.productReviewRequestRepository_
-    );
+    const repo = this.activeManager_.withRepository(this.productReviewRequestRepository_);
 
     const query = buildQuery(selector, config);
 
@@ -104,9 +88,7 @@ class ProductReviewRequestService extends TransactionBaseService {
     selector: Selector<ProductReviewRequest>,
     config: FindConfig<ProductReviewRequest>
   ): Promise<[ProductReviewRequest[], number]> {
-    const repo = this.activeManager_.withRepository(
-      this.productReviewRequestRepository_
-    );
+    const repo = this.activeManager_.withRepository(this.productReviewRequestRepository_);
 
     const query = buildQuery(selector, config);
 
@@ -114,16 +96,12 @@ class ProductReviewRequestService extends TransactionBaseService {
   }
 
   async delete(id: string) {
-    return await this.atomicPhase_(async manager_ => {
-      const repo = manager_.withRepository(
-        this.productReviewRequestRepository_
-      );
+    return await this.atomicPhase_(async (manager_) => {
+      const repo = manager_.withRepository(this.productReviewRequestRepository_);
 
       await repo.delete(id);
 
-      await this.eventBusService
-        .withTransaction(manager_)
-        .emit(ProductReviewRequestService.Events.DELETED, { id });
+      await this.eventBusService.withTransaction(manager_).emit(ProductReviewRequestService.Events.DELETED, { id });
     });
   }
 }
