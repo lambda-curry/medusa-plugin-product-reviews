ccbcd97bda21005a370c47500f9685984c057c7a
Abdullah Afzal <mr.abdullahafzal@gmail.com>
2024-08-08 16:50:40 +0500

    fix(migrations): resolve multiple primary keys error in productReview migration
    - Removed redundant primary key constraint on 'id' column in ALTER TABLE statement.
    - Simplified migration by setting primary key during table creation.

--- packages/plugin/src/migrations/1683131541071-ProductReview.ts
+++ packages/plugin/src/migrations/1683131541071-ProductReview.ts
@@ -4,7 +4,7 @@ export class productReview1683131541071 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `CREATE TABLE IF NOT EXISTS "product_review" (
-        "id" character varying NOT NULL, 
+        "id" character varying NOT NULL PRIMARY KEY, 
         "product_id" character varying NOT NULL, 
         "product_variant_id" character varying,
         "customer_id" character varying NOT NULL,
@@ -15,9 +15,6 @@ export class productReview1683131541071 implements MigrationInterface {
         "deleted_at" TIMESTAMP WITH TIME ZONE )`
     );
     await queryRunner.query(`
-      ALTER TABLE "product_review"
-      ADD CONSTRAINT "PK_product_review" PRIMARY KEY ("id");
-
       ALTER TABLE "product_review"
       ADD CONSTRAINT "FK_product_review_product_id" FOREIGN KEY ("product_id")
       REFERENCES "product" ("id")
